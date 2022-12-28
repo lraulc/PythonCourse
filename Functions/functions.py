1df14aa428b500116b18d1d541a9152f44529040
lraulc <lraul.castillo@gmail.com>
2022-12-27 18:06:08 -0600

    file generator and functions

--- Functions/functions.py
+++ Functions/functions.py
@@ -1,5 +1,16 @@
-def greet():
-    print("Hello!")
-    print("Welcome aboard!!")
+'''
+1 - Perform a Task
+2 - Return a value
+'''
+def greet(name):
+    print(f"Hi {name}")
     
-greet()
+def get_greeting(name):
+    return f"Hi {name}"
+
+
+message = get_greeting("New name")
+
+# Creates and writes to a document called "content.txt"
+file = open("NameFile.txt", "w")
+file.write(message)

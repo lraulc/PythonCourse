15827f1182b86620ea319701f6a621ffae46fd51
lraulc <lraul.castillo@gmail.com>
2022-12-27 18:11:06 -0600

    keyword arguments

---
+++ Functions/incremental.py
@@ -0,0 +1,7 @@
+def increment(number, by):
+    return number + by
+
+# Keywords "number" and "by" used to reference arguments in a function
+# Also called "Keyword arguments"
+
+print(increment(number=2, by=1))
